ce9b13aa6d6d2e582faa3433ad7785ebfef6052d
Eugene Shashkov <evgeny.shashkov@imail2.org>
2023-12-19 09:20:57 -0700

    eugene_test

---
+++ src/test/java/GoogleEugene.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class GoogleEugene {
+    public static void main(String[] args){
+
+        System.setProperty("webdriver.chrome.driver", "/Users/eshashko/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://www.google.com");
+
+        WebElement searchField = driver.findElement(By.name("q"));
+
+        searchField.sendKeys("QA");
+
+        searchField.submit();
+
+        WebElement resultsStatsElement = driver.findElement(By.id("result-stats"));
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(resultsStatsElement));
+
+        String pageText = driver.getPageSource();
+        assertTrue(pageText.contains("Quality assurance"));
+
+        System.out.println("Our test just passed");
+
+        driver.quit();
+
+    }
+}
