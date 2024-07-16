549d8493c44be0987f902ab357cff1f2263f788c
Iaroslava <Iaroslava@MacBook-Pro.local>
2024-07-16 18:36:33 -0500

    yahoo chrome works

--- src/test/java/chromeWork/YahooTestChrome.java
+++ src/test/java/chromeWork/YahooTestChrome.java
@@ -9,17 +9,17 @@ public class YahooTestChrome {
 
     public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/Iaroslava/Downloads/chromedriver-mac-arm64/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/Iaroslava/Downloads/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.yahoo.com/");
 
 //        WebElement searchField = driver.findElement(By.name("p"));
-//        WebElement searchField = driver.findElement(By.id("ybar-sbq"));
-//        WebElement searchField = driver.findElement(By.cssSelector("#ybar-sbq"));
-        WebElement searchField = driver.findElement
-                (By.xpath("/html/body/header/div/div/div/div/div/div[2]/div/div[1]/div[3]/form/input[1]"));
+        WebElement searchField = driver.findElement(By.id("ybar-sbq"));
+////        WebElement searchField = driver.findElement(By.cssSelector("#ybar-sbq"));
+////        WebElement searchField = driver.findElement
+////                (By.xpath("/html/body/header/div/div/div/div/div/div[2]/div/div[1]/div[3]/form/input[1]"));
         searchField.sendKeys("QA");
         searchField.submit();
         Thread.sleep(2000);
